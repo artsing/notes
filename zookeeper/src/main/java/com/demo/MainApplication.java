02f9e6a8d5df277632ee9c547a75bd0e14d77fec
artsing <artsing@yeah.net>
2019-04-30 21:41:58 +0800

    添加zookeeper模块代码

---
+++ zookeeper/src/main/java/com/demo/MainApplication.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] argvs) {
+        SpringApplication.run(MainApplication.class, argvs);
+    }
+}
