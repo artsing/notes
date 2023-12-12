c63ed694be09d9e1d468218951a466a1eec7ea57
artsing <artsing@yeah.net>
2023-12-12 23:40:59 +0800

    update golong.org etc.

---
+++ golang/utls.go
@@ -0,0 +1,9 @@
+go install github.com/ramya-rao-a/go-outline@latest
+go install github.com/acroca/go-symbols@latest
+go install golang.org/x/tools/cmd/guru@latest
+go install golang.org/x/tools/cmd/gorename@latest
+go install github.com/josharian/impl@latest
+go install github.com/rogpeppe/godef@latest
+go install github.com/sqs/goreturns@latest
+go install github.com/golang/lint/golint@latest
+go install github.com/cweill/gotests/gotests@latest
